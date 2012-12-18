6c3eb949ffda326e6de6bf6d37e30d2960af5bdf
Trung Ngo <ndtrung4419@gmail.com>
2012-12-19 00:42:20 +0700

    Affix cho navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function (){
+	$("#nav-wrapper").height($("#navbar").outerHeight(true))
+	var pos = $('#navbar').offset().top + 20;
+	$('#navbar').affix({
+    	offset: pos
+	});
+});
